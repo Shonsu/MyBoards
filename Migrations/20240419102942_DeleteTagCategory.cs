b211f24ce114bc11f9f410143599483b3ebeac43
Shonsu <shonsu82@gmail.com>
2024-04-20 17:33:52 +0200

    delete tag category

---
+++ Migrations/20240419102942_DeleteTagCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteTagCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Tags");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Tags",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
